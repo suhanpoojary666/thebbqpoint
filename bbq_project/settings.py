c5ce8d51fbff471a5686c02b24d7641b764c8911
suhanpoojary666 <suhanpoojary666@gmail.com>
2026-07-16 17:26:53 +0530

    Align default sender with SMTP host user

--- bbq_project/settings.py
+++ bbq_project/settings.py
@@ -157,4 +157,4 @@ EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'thebbqpoint@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'no-reply@thebbqpoint.com')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
